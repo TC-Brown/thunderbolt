5ccfc4085b18b0d537526cf6556c61b0f7f897ef
TC Brown <trrncbrown@gmail.com>
2026-02-15 17:27:01 -0600

    Initial commit for bata version of hub

---
+++ static/js/hub.js
@@ -0,0 +1,11 @@
+const portfolioBtn = document.getElementById('app-tile-1');
+
+portfolioBtn.addEventListener('click', () => {
+    window.open("https://tc-brown.github.io/portfolio/");
+});
+
+const repoBtn = document.getElementById('app-tile-2');
+
+repoBtn.addEventListener('click', () => {
+    window.open("https://github.com/TC-Brown");
+});
