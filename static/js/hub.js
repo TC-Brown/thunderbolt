eca5259f6aecb990c187f9915db327f825da78ca
TC Brown <trrncbrown@gmail.com>
2026-02-15 22:52:48 -0600

    Modified hub.js

--- static/js/hub.js
+++ static/js/hub.js
@@ -2,8 +2,7 @@ const portfolioBtn = document.getElementById('app-tile-1');
 
 portfolioBtn.addEventListener('click', () => {
     window.open("portfolio/index.html");
-});
-
+})
 const repoBtn = document.getElementById('app-tile-2');
 
 repoBtn.addEventListener('click', () => {
