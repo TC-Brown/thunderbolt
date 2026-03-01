1bb6da7286f784925f206ca573aa95688f269f88
TC Brown <trrncbrown@gmail.com>
2026-03-01 05:10:29 -0600

    worked on POS app

--- app/inventory/inventory.js
+++ app/inventory/inventory.js
@@ -118,7 +118,7 @@ function renderTables() {
                 totalStr = `${wtTotal}`;
             }
             else {
-                totalStr = `Item not found`;     /* get material .ico for deleter button */
+                totalStr = `Item not found`;     /* get material .ico for delete button */
             }
 
             return `
@@ -127,7 +127,9 @@ function renderTables() {
                             ${loc === 'Walk-in' ? `<td>${i.bins}</td>` : ''}
                             ${loc !== 'Wing' ? `<td>${i.cases}</td>` : ''}
                             <td>${i.bags}</td>
-                            <td><div class="delete-btn" onclick="deleteItem(${i.id})" style="background:#e74c3c"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></div></td>
+                            <td>
+                                <div class="delete-btn" onclick="deleteItem(${i.id})" style="background:#e74c3c"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="12px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></div>
+                            </td>
                             <td>${totalStr}</td>
                         </tr>
                         `;
@@ -147,7 +149,6 @@ function clearForm() {
     document.getElementById('cases').value = '';
     document.getElementById('bags').value = '';
 }
-
 // Initial Load
 toggleInputs();
 renderTables();
