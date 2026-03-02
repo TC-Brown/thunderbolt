29d4e8da1a4125e59ffc356d8323398047558d8d
TC Brown <trrncbrown@gmail.com>
2026-03-02 11:55:54 -0600

    [v-2.0.0]

--- static/js/hub.js
+++ static/js/hub.js
@@ -1,23 +1,25 @@
-/*-- Project ID: 0001 --*/
-const portfolioBtn = document.getElementById('app-tile-1');
+/*-- Project ID: 260226 --*/
+const inventoryBtn = document.getElementById('app-tile-260226');
+
+inventoryBtn.addEventListener('click', () => {
+    window.location.href = "app/inventory/index.html";
+});
+
+/*-- Project ID: 260302 --*/
+const portfolioBtn = document.getElementById('app-tile-260302');
 
 portfolioBtn.addEventListener('click', () => {
     window.location.href = "app/portfolio/index.html";
 });
 
-/*-- Project ID: 0002 --*/
-const posBtn = document.getElementById('app-tile-2');
 
-posBtn.addEventListener('click', () => { 
-    window.location.href = "app/POS/index.html";
+/* -- Development page navigation --*/
+const devBtn = document.getElementById('app-tile-dev');
+
+devBtn.addEventListener('click', () => { 
+    if(confirm("The page you are trying to access is under development. Do you want you to continue?") == true) {
+        window.location.href = "app/POS/index.html";
+    } else {
+        window.location.href = "index.html";
+    };
 });
-
-/*-- Project ID: 0004 --*/
-const inventoryBtn = document.getElementById('app-tile-4');
-
-inventoryBtn.addEventListener('click', () => {
-    window.location.href = "app/inventory/index.html";
-});
-/*-- Project ID: 0005 --*/
-/*-- Project ID: 0006 --*/
-/*---- Functions ----*/
